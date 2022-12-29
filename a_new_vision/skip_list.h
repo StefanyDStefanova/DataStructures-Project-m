0886e77f57e0750de493054d895214095e55c233
StefanyDStefanova <102753112+StefanyDStefanova@users.noreply.github.com>
2022-12-29 19:17:24 +0200

    Update skip_list.h

--- a_new_vision/skip_list.h
+++ a_new_vision/skip_list.h
@@ -16,6 +16,11 @@ private:
 	void copy(SkipList<T> const& l);
 	void erase();
 
+	SkipListNode<T>* getNodeAt(int index);
+	//print final queue of cities 
+	void printQueue(std::queue<T> q);
+
+
 public:
 	SkipList() : frontL(nullptr), endL(nullptr) {}
 	SkipList(SkipList<T> const& sl);
@@ -34,20 +39,18 @@ public:
 	T& getAt(SkilListIterator<T> const& it);
 
 	bool insertElem(T const& x);
-	bool deleteElem(T& x);
+	//bool deleteElem(T& x);
+	bool deleteQueue();
 
 	//take pointer from the first to the second city -> this is skip field
-	//void connect(const std::string arr[2], const SkipList<T> list);
-	void connect(int first, int second);
-	
-	// helper fuction for create "edge" between cities
-	//SkipListNode<T>* indexOf(std::string city, const SkipList<T> list);
-	int indexOf(T& city);
-	
+	bool connect(T& first, T& second);
+
+	// helper fuction for give index of city from list
+	int indexOf(T& city);	
+
 	//creates a path from the available cities taking the shortest path with the most important ones 
-	void wayForAnyaAndVankata(const SkipList<T> list, const std::string importantCities);
-	//print final queue of cities 
-	void printQueue(std::queue<T> q);
+	void wayForAnyaAndVankata(std::queue<T> importantCities);
+
 };
 
 
